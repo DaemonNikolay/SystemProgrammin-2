2a4d74fb4224d933f8213bb0307e308cbab7bc68
Nikolay Eckert <nikolay-eckert@mail.ru>
2019-09-15 20:11:38 +0700

    Init project; Create the task one

---
+++ SystemProgramming/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Collections.Generic;
+
+
+namespace Task1
+{
+	class Program
+	{
+		private static readonly int limitProcess = 5;
+
+		static void Main(string[] args)
+		{
+			var notepadsProcess = new List<Process>();
+			for (var i = 0; i < limitProcess; i++)
+			{
+				notepadsProcess.Add(StartNotepadProcess());
+			}
+
+			if (notepadsProcess.Count == 0)
+			{
+				return;
+			}
+
+			Thread.Sleep(TimerMS());
+
+			foreach (var notepadProcess in notepadsProcess)
+			{
+				CloseNotepad(notepadProcess);
+			}
+		}
+
+		private static Process StartNotepadProcess()
+		{
+			return Process.Start("notepad.exe");
+		}
+
+		private static int TimerMS()
+		{
+			return 2000;
+		}
+
+		private static void CloseNotepad(Process processNotepad)
+		{
+			if (processNotepad == null || processNotepad.HasExited)
+			{
+				return;
+			}
+
+			processNotepad.CloseMainWindow();
+		}
+	}
+}
+
