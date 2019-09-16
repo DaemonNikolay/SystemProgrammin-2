8684ceb5f54fdb57ea7389fe8c56dff9d891733a
Nikolay Eckert <nikolay-eckert@mail.ru>
2019-09-16 13:29:14 +0700

    Create the task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+
+namespace Task4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var hdDirectoryInWhichToSearch = new DirectoryInfo(@"c:\");
+			FileSystemInfo[] files = new FileSystemInfo[0];
+			try
+			{
+				files = hdDirectoryInWhichToSearch.GetFileSystemInfos();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"ERROR: {e.Message}");
+			}
+
+			if (files.Length > 0)
+			{
+				PrintFilesInfoDirectory(files);
+			}
+		}
+
+		private static void PrintFilesInfoDirectory(FileSystemInfo[] files)
+		{
+			foreach (var file in files)
+			{
+				Console.WriteLine($"{file.FullName} \n" +
+								$"\tДата создания: {file.CreationTime} \n" +
+								$"\tРасширение: {file.Extension} \n" +
+								$"\tДата последнего открытия: {file.LastAccessTime} \n" +
+								$"\tДата последнего изменения: {file.LastWriteTime} \n\n");
+			}
+		}
+	}
+}
