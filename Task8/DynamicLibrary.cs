2acb874be1042d54aba4f9ece5de235d8f8cce90
Nikolay Eckert <nikolay-eckert@mail.ru>
2019-12-14 01:14:49 +0700

    Create the lab 8

---
+++ Task8/DynamicLibrary.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace Task8
+{
+	public class DynamicLibrary
+	{
+		public static void PrintConsole(string content) => Console.Write(content);
+	}
+}
---
+++ Task8/UsingDynamicLibrary.cs
@@ -0,0 +1,10 @@
+﻿namespace Task8
+{
+	class UsingDynamicLibrary
+	{
+		public static void Main()
+		{
+			DynamicLibrary.PrintConsole("y943y943");
+		}
+	}
+}
