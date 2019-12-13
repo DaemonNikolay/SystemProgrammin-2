aa4569b9c7f0784df7ec553174a49efeb503bd25
Nikolay Eckert <nikolay-eckert@mail.ru>
2019-12-14 00:01:40 +0700

    Create the lab 3

---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System;
+
+namespace Task3
+{
+	class Program
+	{
+		private static readonly int limitProcess = 5;
+
+		static void Main(string[] args)
+		{			
+			Thread.Sleep(TimerMS(3));
+
+			StartNotepadProcess();
+		}
+
+		private static Process StartNotepadProcess()
+		{
+			return Process.Start("notepad.exe");
+		}
+
+		private static int TimerMS(int second)
+		{
+			return Math.Abs(1000 * second);
+		}
+	}
+}
+
